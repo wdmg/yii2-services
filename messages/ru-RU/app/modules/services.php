22cf59658c3dbc7c89b18e99358de0110fc03150
alex-wdmg <wdmg.com.ua@gmail.com>
2019-05-22 01:44:40 +0300

    Added transpalations

--- messages/ru-RU/app/modules/services.php
+++ messages/ru-RU/app/modules/services.php
@@ -5,6 +5,24 @@ return [
     'Service' => 'Сервис',
     'Services' => 'Сервисное обслуживание',
 
+    'Restore directory rights' => 'Восстановить права',
+    'Clear runtime cache' => 'Очистить runtime кэш',
+    'Clear web cache' => 'Очистить web кэш',
+    'Clear the system cache' => 'Очистить системный кеш',
+
+
+    'Writing rights (0777) successfully installed!' => 'Права на запись (0777) успешно установлены!',
+    'Error setting write permissions (0777).' => 'Ошибка установки прав на запись (0777).',
+    'Execution rights (0755) successfully installed!' => 'Права на исполнение (0755) успешно установлены!',
+    'Error setting permissions to execute (0755).' => 'Ошибка установки прав на исполнение (0755).',
+    'The system cache has been successfully cleared!' => 'Кеш системы был успешно очищен!',
+    'Error clearing the system cache.' => 'Ошибка очистки кеша системы.',
+    'Web-assets have been successfully cleared!' => 'Web-assets был успешно очищен!',
+    'Error clearing the web assets cache. Folder {filename} could not be deleted.' => 'Ошибка очистки кеша web-assets. Папку {filename} не удалось удалить.',
+    'Runtime has been successfully cleaned!' => 'Runtime был успешно очищен!',
+    'Error clearing runtime cache. Folder {filename} could not be deleted.' => 'Ошибка очистки кеша runtime. Папку {filename} не удалось удалить.',
+    'Folder {filename} could not be deleted.' => 'Папку {filename} не удалось удалить.',
+
 ];
 
 ?>
